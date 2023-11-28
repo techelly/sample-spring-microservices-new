ad8b2811332bc1a903ecdfcd80c0965defd607ed
prabhat1510 <prabhatc@gmail.com>
2023-11-28 15:24:37 +0530

    Microservices example using Spring cloud

---
+++ organization-service/src/main/java/com/techelly/services/organization/client/DepartmentClient.java
@@ -0,0 +1,20 @@
+package com.techelly.services.organization.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.techelly.services.organization.model.Department;
+
+@FeignClient(name = "department-service")
+public interface DepartmentClient {
+
+	@GetMapping("/organization/{organizationId}")
+	public List<Department> findByOrganization(@PathVariable("organizationId") Long organizationId);
+	
+	@GetMapping("/organization/{organizationId}/with-employees")
+	public List<Department> findByOrganizationWithEmployees(@PathVariable("organizationId") Long organizationId);
+	
+}
